350b0b240aa012e0a4298b09b36a4ba044d711e7
krish_soni <krishsoni198@gmail.com>
2026-04-09 21:16:29 +0530

    09/04/2026 2.0

--- app/src/main/java/com/example/sahaysathi/ui/volunteer/notifications/ApplicationModel.java
+++ app/src/main/java/com/example/sahaysathi/ui/volunteer/notifications/ApplicationModel.java
@@ -16,21 +16,12 @@ public class ApplicationModel {
     public String getApplicationId() { return applicationId; }
     public void setApplicationId(String applicationId) { this.applicationId = applicationId; }
     public String getEventId() { return eventId; }
-    public void setEventId(String eventId) { this.eventId = eventId; }
-    public String getNgoId() { return ngoId; }
-    public void setNgoId(String ngoId) { this.ngoId = ngoId; }
     public String getUserId() { return userId; }
     public void setUserId(String userId) { this.userId = userId; }
     public String getStatus() { return status; }
-    public void setStatus(String status) { this.status = status; }
-    public long getTimestamp() { return timestamp; }
-    public void setTimestamp(long timestamp) { this.timestamp = timestamp; }
     public String getEventName() { return eventName; }
-    public void setEventName(String eventName) { this.eventName = eventName; }
 
     public String getlocation() { return location; }
-    public void setlocation(String location) { this.location = location; }
 
     public String getInstructions() { return instructions; }
-    public void setInstructions(String instructions) { this.instructions = instructions; }
 }
